464cdf28fc692637f72a34ee20a67d5c37a51899
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-18 04:28:23 +0700

    Create SmsUtils.kt

---
+++ app/src/main/java/com/example/myapp/utils/SmsUtils.kt
@@ -0,0 +1,9 @@
+package com.example.myapp.utils
+
+import android.content.Context
+
+object SmsUtils {
+    fun getSmsData(context: Context) {
+        // Logika untuk mendapatkan data SMS
+    }
+}
