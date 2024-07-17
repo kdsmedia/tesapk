82c92817b4dda1ca622c3464e75c9a954396f37f
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-18 04:23:42 +0700

    Update ExampleUnitTest.kt

--- app/src/test/java/com/example/myapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapp/ExampleUnitTest.kt
@@ -1,1 +1,11 @@
-zczczczczc
+package com.example.myapp
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
