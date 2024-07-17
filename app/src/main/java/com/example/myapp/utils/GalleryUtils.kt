9ff3c1583319f1495f39d9fa04ead2bd8071bbf6
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-18 04:28:44 +0700

    Create GalleryUtils.kt

---
+++ app/src/main/java/com/example/myapp/utils/GalleryUtils.kt
@@ -0,0 +1,9 @@
+package com.example.myapp.utils
+
+import android.content.Context
+
+object GalleryUtils {
+    fun getGalleryData(context: Context) {
+        // Logika untuk mendapatkan data galeri
+    }
+}
